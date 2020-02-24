4a808513fbdafe74c4ed64afbbb35ea8dbd8c838
g000001 <chiba.masaomi@gmail.com>
2020-02-24 17:02:53 +0900

    add "https://github.com/g000001/srfi-172#functional" package

--- package.lisp
+++ package.lisp
@@ -11,10 +11,10 @@
    "https://github.com/g000001/srfi-13"
    "https://github.com/g000001/srfi-16"
    "https://github.com/g000001/srfi-23"
+   "https://github.com/g000001/srfi-34"
    "https://github.com/g000001/srfi-39"
    "https://github.com/g000001/srfi-45"
    quasiquote1
-   srfi-34
    rnrs)
   (:shadowing-import-from
    "https://github.com/g000001/srfi-45"
@@ -85,6 +85,52 @@
   with-exception-handler write-bytevector write-char write-string
   write-u8 zero?)
   (:export srfi-172-syntax))
+
+
+(defpackage "https://github.com/g000001/srfi-172#functional"
+  (:use "https://github.com/g000001/srfi-172")
+  (:export srfi-172-syntax)
+  (:export
+   cond-expand asin char->integer char-whitespace?  square
+   eof-object char-ci>?  list->vector char<=?  char>=?  error-object?
+   truncate/ string->symbol quasiquote char-alphabetic?  truncate
+   rational?  not log do cos memq integer?  make-vector eqv?  finite?
+   cond truncate-remainder string-map or string-copy char-ci<=?
+   append procedure?  string=?  bytevector-u8-ref positive?  for-each
+   list-copy string>=?  denominator symbol?  unquote-splicing cdaddr
+   cdadar caaddr caadar with-exception-handler char-ci=?  if list-ref
+   and list-tail vector->string unquote list->string utf8->string
+   make-string parameterize open-input-string digit-value string?
+   open-input-bytevector => equal?  bytevector-length eof-object?
+   exact?  char=?  char<?  ceiling char-ci>=?  bytevector?
+   error-object-message floor-remainder let bytevector-append >= <=
+   open-output-bytevector modulo floor/ rationalize cdadr
+   char-foldcase caaar string-ci=?  bytevector
+   call-with-current-continuation open-output-string memv cdar real?
+   cdaadr cdaaar caar cddadr cddaar caaadr integer->char caaaar
+   newline cadadr let-values string<?  exact-integer-sqrt
+   char-upper-case?  apply make-promise cdr floor-quotient string<=?
+   vector-ref get-output-string infinite?  string-length string->list
+   cddar raise-continuable negative?  char-downcase cadar
+   char-numeric?  call-with-values string error-object-irritants
+   string->utf8 lambda lcm inexact?  gcd magnitude string-append
+   make-rectangular cadr cdddr let*-values case-lambda caddr guard
+   make-parameter string-ci<?  char>?  assoc expt error let* cddr atan
+   assv exact null?  even?  sqrt imag-part real-part vector char?
+   cons char-ci<?  delay cdaar begin else caadr raise list when abs
+   substring force round angle pair?  floor symbol->string odd?  zero?
+   make-polar vector-copy nan?  vector-map vector-for-each quotient
+   map vector-length boolean=?  boolean?  get-output-bytevector
+   letrec* char-upcase exact-integer?  quote max vector->list
+   string->vector complex?  reverse remainder letrec tan sin values
+   promise?  min eq?  car assq symbol=?  inexact delay-force call/cc
+   unless make-bytevector length string->number vector-append list?
+   char-lower-case?  numerator truncate-quotient number?  string-ref
+   string-ci>?  make-list string-foldcase number->string
+   bytevector-copy acos string-upcase case cadaar member cddddr
+   vector?  cdddar string>?  cadddr caddar exp > = < + / - *
+   string-for-each string-ci<=?  string-downcase string-ci>=?
+   dynamic-wind))
 
 
 (defpackage "https://github.com/g000001/srfi-172#internals"
